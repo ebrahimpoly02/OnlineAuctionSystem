7e8cdbaeacb158f9a0cc9d9a843ce90030d35993
Ubuntu <ubuntu@ip-172-31-9-5.me-south-1.compute.internal>
2025-12-16 18:39:08 +0000

    Fixed duplicated code in consumers.py

--- auctions/consumers.py
+++ auctions/consumers.py
@@ -22,30 +22,6 @@ class AuctionConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
-import json
-from channels.generic.websocket import AsyncWebsocketConsumer
-from channels.db import database_sync_to_async
-from django.utils import timezone
-
-class AuctionConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        self.auction_id = self.scope['url_route']['kwargs']['auction_id']
-        self.room_group_name = f'auction_{self.auction_id}'
-
-        # Join room group
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
-
-        await self.accept()
-
-    async def disconnect(self, close_code):
-        # Leave room group
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
 
     # Receive message from WebSocket
     async def receive(self, text_data):
